1a43373057d94729056b2c8162b9140dc75a6d31
Michele Minelli <Michele.Minelli@sony.com>
2020-03-06 15:15:17 +0100

    Support both PC and Android.

--- src/include/openabe/openabe.h
+++ src/include/openabe/openabe.h
@@ -1,21 +1,21 @@
-/// 
+///
 /// Copyright (c) 2018 Zeutro, LLC. All rights reserved.
-/// 
+///
 /// This file is part of Zeutro's OpenABE.
-/// 
+///
 /// OpenABE is free software: you can redistribute it and/or modify
 /// it under the terms of the GNU Affero General Public License as published by
 /// the Free Software Foundation, either version 3 of the License, or
 /// (at your option) any later version.
-/// 
+///
 /// OpenABE is distributed in the hope that it will be useful,
 /// but WITHOUT ANY WARRANTY; without even the implied warranty of
 /// MERCHANTABILITY or FITNESS FOR A PARTICULAR PURPOSE.  See the
 /// GNU Affero General Public License for more details.
-/// 
+///
 /// You should have received a copy of the GNU Affero General Public
 /// License along with OpenABE. If not, see <http://www.gnu.org/licenses/>.
-/// 
+///
 /// You can be released from the requirements of the GNU Affero General
 /// Public License and obtain additional features by purchasing a
 /// commercial license. Buying such a license is mandatory if you
@@ -104,6 +104,10 @@ typedef uint32_t OpenABESecurityLevel;
 //
 // Core library header files
 //
+
+#ifdef ANDROID
+#include <openabe/utils/zandroid.h>
+#endif
 
 #include <openabe/zobject.h>
 #include <openabe/utils/zconstants.h>
--- src/include/openabe/utils/zinteger.h
+++ src/include/openabe/utils/zinteger.h
@@ -1,21 +1,21 @@
-/// 
+///
 /// Copyright (c) 2018 Zeutro, LLC. All rights reserved.
-/// 
+///
 /// This file is part of Zeutro's OpenABE.
-/// 
+///
 /// OpenABE is free software: you can redistribute it and/or modify
 /// it under the terms of the GNU Affero General Public License as published by
 /// the Free Software Foundation, either version 3 of the License, or
 /// (at your option) any later version.
-/// 
+///
 /// OpenABE is distributed in the hope that it will be useful,
 /// but WITHOUT ANY WARRANTY; without even the implied warranty of
 /// MERCHANTABILITY or FITNESS FOR A PARTICULAR PURPOSE.  See the
 /// GNU Affero General Public License for more details.
-/// 
+///
 /// You should have received a copy of the GNU Affero General Public
 /// License along with OpenABE. If not, see <http://www.gnu.org/licenses/>.
-/// 
+///
 /// You can be released from the requirements of the GNU Affero General
 /// Public License and obtain additional features by purchasing a
 /// commercial license. Buying such a license is mandatory if you
@@ -33,6 +33,10 @@
 
 #ifndef __ZINTEGER_H__
 #define __ZINTEGER_H__
+
+#ifdef ANDROID
+#include "zandroid.h"
+#endif
 
 #ifndef openabe_ZObject_h
 #include "zobject.h"
@@ -137,4 +141,3 @@ private:
 }
 
 #endif	// __ZINTEGER_H__
-
